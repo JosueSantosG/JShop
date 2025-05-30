3fafe21f0c265c0e6242a0b4ff0dab40f2238708
JosueSantosG <josue.santosgonzabay@upse.edu.ec>
2025-05-30 18:46:58 -0500

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  outDir: './dist', // Directorio de salida explícito
+  output: 'static', // Modo de salida estática (necesario para Vercel)
   vite: {
     plugins: [tailwindcss()]
   }
